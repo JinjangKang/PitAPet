19de2a7db2ac3b8bc3b47c7c3dd5e3948dd77755
JinjangKang <wkdwlsrkd97@gmail.com>
2023-09-06 13:34:59 +0900

    [fix] port: 20000

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { Idle } from './idle/Idle.entity';
 export const dataSource = new DataSource({
     type: 'mariadb', //Database 설정
     host: '3.37.127.237',
-    port: 3306,
+    port: 20000,
     username: 'jinkang',
     password: '0408',
     database: 'pitapet',
